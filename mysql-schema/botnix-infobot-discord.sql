ac342eeed5d8cdf9129b26e442dc73510d22fc93
Craig Edwards <craigedwards@brainbox.cc>
2019-11-14 09:00:04 +0000

    add javascript tables, triggers and stored proc

--- mysql-schema/botnix-infobot-discord.sql
+++ mysql-schema/botnix-infobot-discord.sql
@@ -52,3 +52,55 @@ ALTER TABLE `infobot_discord_user_cache`
 
 ALTER TABLE `infobot_discord_list_sites`
 MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+
+DELIMITER $$
+CREATE DEFINER=`admin`@`localhost` PROCEDURE `check_max` (IN `_guild_id` BIGINT(20) UNSIGNED)  READS SQL DATA
+    SQL SECURITY INVOKER
+    COMMENT 'Throws an error if the guild_id has more than 100 kv pairs'
+BEGIN
+DECLARE ERR_STATUS_NOT_VALID CONDITION FOR SQLSTATE '45000';
+DECLARE matches INT(11) UNSIGNED;
+SELECT COUNT(keyname) INTO matches FROM infobot_javascript_kv WHERE infobot_javascript_kv.guild_id = _guild_id;
+IF matches > 100 THEN
+    SET @text = CONCAT('No more KV storage allowed for guild id ', new.guild_id);
+    SIGNAL ERR_STATUS_NOT_VALID SET MESSAGE_TEXT = @text;
+END IF;
+END$$
+
+DELIMITER ;
+
+CREATE TABLE `infobot_discord_javascript` (
+  `id` bigint(20) UNSIGNED NOT NULL COMMENT 'Channel ID',
+  `created` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Creation date',
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Last modified date',
+  `last_exec_ms` float NOT NULL DEFAULT 0 COMMENT 'Last execution time',
+  `last_compile_ms` float NOT NULL DEFAULT 0 COMMENT 'Last compile time in ms',
+  `script` longtext CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'Actual javascript content',
+  `last_error` text CHARACTER SET utf8mb4 DEFAULT NULL COMMENT 'Last error message or empty/null',
+  `last_memory_max` int(11) NOT NULL DEFAULT 0 COMMENT 'Last memory usage of script executed',
+  `dirty` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Set to 1 if the bot is to reload this script'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Information on which channels are using javascript replies';
+
+CREATE TABLE `infobot_javascript_kv` (
+  `guild_id` bigint(20) UNSIGNED NOT NULL COMMENT 'Guild ID',
+  `keyname` varchar(100) NOT NULL COMMENT 'Key name',
+  `value` text NOT NULL COMMENT 'Value content'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Key/Value store for javascript users';
+DELIMITER $$
+CREATE TRIGGER `insert_check_max` BEFORE INSERT ON `infobot_javascript_kv` FOR EACH ROW CALL check_max(new.guild_id)
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `update_check_max` BEFORE UPDATE ON `infobot_javascript_kv` FOR EACH ROW CALL check_max(new.guild_id)
+$$
+DELIMITER ;
+
+ALTER TABLE `infobot_discord_javascript`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `dirty` (`dirty`),
+  ADD KEY `created` (`created`),
+  ADD KEY `updated` (`updated`);
+
+ALTER TABLE `infobot_javascript_kv`
+  ADD PRIMARY KEY (`guild_id`,`keyname`);
+
