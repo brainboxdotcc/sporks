7a9c264fc4cc1066353cebe8529f565f91bfdf5d
Craig Edwards <craigedwards@brainbox.cc>
2019-12-23 09:44:27 +0000

    remove from top level

--- statusfield.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <sporks/statusfield.h>
-
-/**
- * Constructor for statusfield, contains a simple field definition for an embed stored as JSON
- */
-statusfield::statusfield(const std::string &a, const std::string &b) : name(a), value(b) {
-}
-
