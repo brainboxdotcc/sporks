5a20815fa7ed22440e242c9a97d8e5b86eca5280
Craig Edwards <craigedwards@brainbox.cc>
2020-01-10 09:09:04 +0000

    fix to use -a because heirloom-mailx is gone replaced with bsd-mailx

--- mail-core-file.sh
+++ mail-core-file.sh
@@ -18,5 +18,5 @@
 #
 cd $1
 # run gdb in batch mode to generate mail the stack trace of the latest crash to the error_recipeint from config.json
-/usr/bin/gdb -batch -ex "set pagination off" -ex "set height 0" -ex "set width 0" -ex "bt full" ./bot `ls -Art *core* | tail -n 1` | grep -v ^"No stack."$ | mail -s "Sporks Bot rebooted, stack trace and log attached" `/usr/bin/jq -r '.error_recipient' ../config.json` -A log/aegis.log
+/usr/bin/gdb -batch -ex "set pagination off" -ex "set height 0" -ex "set width 0" -ex "bt full" ./bot `ls -Art *core* | tail -n 1` | grep -v ^"No stack."$ | mail -s "Sporks Bot rebooted, stack trace and log attached" `/usr/bin/jq -r '.error_recipient' ../config.json` -a log/aegis.log
 
